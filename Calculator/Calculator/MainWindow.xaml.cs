2e73273c5867256b4d669d0f8b2c716f69a5c1cf
WhiteX0 <147401090+WhiteX0@users.noreply.github.com>
2024-04-28 16:00:50 +0200

    Add files via upload

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+
+        double numb1;
+        double numb2;
+        char op;
+        double ergebnis;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            textbox.Text += btn.Content.ToString();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text = "";
+        }
+
+        private void multiplikation_Click(object sender, RoutedEventArgs e)
+        {
+            ergebnis = numb1 * numb2;
+        }
+    }
+}
